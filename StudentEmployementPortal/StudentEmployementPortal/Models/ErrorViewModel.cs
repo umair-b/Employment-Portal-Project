0f203f263050d1328da3214151fec14545e8c7f9
Umair Bham <81626189+umair-b@users.noreply.github.com>
2023-05-04 10:46:53 +0200

    Deleted Files

--- StudentEmployementPortal/StudentEmployementPortal/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace StudentEmployementPortal.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
