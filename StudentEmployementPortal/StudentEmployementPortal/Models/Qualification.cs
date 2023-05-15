2a0d3fb3b47d77d30953d218ffc57381bd5dfea0
Leslie Fabishilaki <leslie.fab85@gmail.com>
2023-05-16 01:45:36 +0200

    added model classes

---
+++ StudentEmployementPortal/StudentEmployementPortal/Models/Qualification.cs
@@ -0,0 +1,12 @@
+﻿namespace StudentEmployementPortal.Models
+{
+    public class Qualification
+    {
+        public int QualificationId { get; set; }
+        public string Institution { get; set; }
+        public string QualificationType { get; set; }
+        public string Majors { get; set; }
+        public string Submajors { get; set; }
+        public string Research { get; set; }
+    }
+}
---
+++ StudentEmployementPortal/StudentEmployementPortal/Models/WorkExperience.cs
@@ -0,0 +1,11 @@
+﻿namespace StudentEmployementPortal.Models
+{
+    public class WorkExperience
+    {
+        public int WorkExperienceId { get; set; }
+        public string Employer { get; set; }
+        public DateOnly Date { get; set; }
+        public string JobTitle { get; set; }
+        public string TasksAndResps { get; set; }
+    }
+}
