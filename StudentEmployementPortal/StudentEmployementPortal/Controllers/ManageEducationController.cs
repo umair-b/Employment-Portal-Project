2de072c6593758196506518275ffe046ecbab635
Umair Bham <81626189+umair-b@users.noreply.github.com>
2023-06-20 15:41:55 +0200

    Added Controllers

---
+++ StudentEmployementPortal/StudentEmployementPortal/Controllers/ManageEducationController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace StudentEmployementPortal.Controllers
+{
+    public class AddEducationController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ StudentEmployementPortal/StudentEmployementPortal/Controllers/ManageQualificationController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace StudentEmployementPortal.Controllers
+{
+    public class AddQualification : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
