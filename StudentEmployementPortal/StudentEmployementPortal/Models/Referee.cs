a5d5b08954f7cc6735d4b0679944d3ab663995b5
Umair Bham <81626189+umair-b@users.noreply.github.com>
2023-07-03 18:29:56 +0200

    More Validation

--- StudentEmployementPortal/StudentEmployementPortal/Models/Referee.cs
+++ StudentEmployementPortal/StudentEmployementPortal/Models/Referee.cs
@@ -10,18 +10,23 @@ namespace StudentEmployementPortal.Models
         public int RefereeId { get; set; }
 
         [DisplayName("Name and Surname")]
+        [Required]
         public string Name { get; set; }
 
         [DisplayName("Job Title")]
+        [Required]
         public string JobTitle { get; set; }
 
         [DisplayName("Institution")]
+        [Required]
         public string Institution { get; set; }
 
         [DisplayName("Cellphone Number")]
+        [Required]
         public int CellNumber { get; set; }
 
         [DisplayName("Email Address")]
+        [Required]
         public string Email { get; set; }
 
         //Navigation Property
